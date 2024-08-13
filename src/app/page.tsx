add28a5aec09f39c6073eb461211f2ecfe3264cb
Chandan Singh <46326929+chandansingh01@users.noreply.github.com>
2024-08-13 22:53:43 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full p-4 relative">
-
-      {/* <CardGrid /> */}
-      {/* <Modal isOpen={true}/> */}
+      <Link href="/dashboard">
+        <h2>Go to dashboard</h2>
+      </Link>
     </div>
-
   );
 }
